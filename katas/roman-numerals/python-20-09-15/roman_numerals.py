60f42515a66b6bcca86fdd59fecccfc62d8dcec2
alejandrodob <alejandrodob@gmail.com>
2015-09-20 16:40:40 +0200

    Time ended: tests are in red...

--- katas/roman-numerals/python-20-09-15/roman_numerals.py
+++ katas/roman-numerals/python-20-09-15/roman_numerals.py
@@ -5,20 +5,39 @@ ROMAN_TEN = 'X'
 FIVE = 5
 TEN = 10
 
+VALUES = {'I': 1, 'V': 5, 'X': 10}
+
 class Roman(object):
 
     @classmethod
     def translate(cls, number):
-        if number == 19:
-            return ROMAN_TEN + ROMAN_ONE + ROMAN_TEN
-        if cls._is_around_fifteen(number):
-            number = number - TEN
-            return ROMAN_TEN + (FIVE - number) * ROMAN_ONE + ROMAN_FIVE + (number - FIVE) * ROMAN_ONE
+        if number <= 3:
+            return ROMAN_ONE * number
+
+        left_part = cls._biggest_roman_symbol_in(number)
+        left_part_value = cls._value_for_roman(left_part)
+        return left_part + cls._direct_translation_for(number-left_part_value)
+
+    @classmethod
+    def _direct_translation_for(cls, number):
         if cls._is_around_ten(number):
-            return (TEN - number) * ROMAN_ONE + ROMAN_TEN + (number - TEN) * ROMAN_ONE
+            return (TEN - number) * ROMAN_ONE + ROMAN_TEN
         if cls._is_around_five(number):
-            return (FIVE - number) * ROMAN_ONE + ROMAN_FIVE + (number - FIVE) * ROMAN_ONE
-        return ROMAN_ONE * number
+            return (FIVE - number) * ROMAN_ONE + ROMAN_FIVE
+
+
+
+    @classmethod
+    def _biggest_roman_symbol_in(cls, number):
+        if number >= TEN:
+            return ROMAN_TEN
+        if number >= FIVE:
+            return ROMAN_FIVE
+        return ROMAN_ONE
+
+    @classmethod
+    def _value_for_roman(cls, roman_symbol):
+        return VALUES[roman_symbol]
 
     @classmethod
     def _is_around_fifteen(cls, number):
