88480f1dc5434489feb9db9d102bdecc8017cd7b
alejandrodob <alejandrodob@gmail.com>
2015-09-21 20:06:44 +0200

    Up to 5

--- katas/roman-numerals/python-21-09-15/roman_numerals.py
+++ katas/roman-numerals/python-21-09-15/roman_numerals.py
@@ -1,7 +1,12 @@
 ONE = 'I'
+FIVE = 'V'
 
 class Roman(object):
 
     @classmethod
     def translate(cls, number):
+        if number == 5:
+            return FIVE
+        if number >= 4:
+            return ONE + FIVE
         return number * ONE
--- katas/roman-numerals/python-21-09-15/spec/roman_numerals_spec.py
+++ katas/roman-numerals/python-21-09-15/spec/roman_numerals_spec.py
@@ -11,3 +11,9 @@ with describe(Roman):
 
     with it("translates 3 to III"):
         expect(Roman.translate(3)).to(equal('III'))
+
+    with it("translates 4 to IV"):
+        expect(Roman.translate(4)).to(equal('IV'))
+
+    with it("translates 5 to V"):
+        expect(Roman.translate(5)).to(equal('V'))
